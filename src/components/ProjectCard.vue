75f43ea3c7b7644eec7973256e8e662cc3786ad6
MatteoBarducci <mbardu1301@gmail.com>
2024-06-14 13:45:57 +0200

    MILESTONE 5

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        projectInfo: Object
+    }
+}
+</script>
+
+<template>
+    <div class="card text-bg-primary mb-3 mx-5" style="width: 25%">
+        <div class="card-body">
+            <h5 class="card-title">{{ projectInfo.titolo }}</h5>                        
+            <p class="card-text">{{ projectInfo.descrizione }}</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+@use '../style/generic';
+
+
+</style>
---
+++ src/components/Projects.vue
@@ -0,0 +1,49 @@
+<script>
+import ProjectCard from './ProjectCard.vue';
+import axios from 'axios';
+
+
+export default {
+    components: {
+        ProjectCard,
+    },
+
+    name: 'Projects',
+
+    data() {
+        return{
+            projects: []
+        };
+    },
+
+    methods: {
+        getProjectsFromApi() {
+            axios.get('http://127.0.0.1:8000/api/projects')
+            .then((response) => {
+                this.projects = response.data.results;
+            });
+        }
+    },
+    mounted() {
+        this.getProjectsFromApi();
+    },
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1>Projects</h1>
+
+        <div class="container text-center d-flex flex-wrap">
+            <div v-for="project in projects">
+                <ProjectCard :projectInfo="project"></ProjectCard>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+@use '../style/generic';
+
+
+</style>
